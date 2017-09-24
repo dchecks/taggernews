ed51a5bea964d34073bb5d46c3020d15c807edb4
Dan Checketts <dan.checketts@mahifx.com>
2017-09-24 23:34:34 +1300

    Adding in analyze_hn, expanding readme, changed from file based to db based articles, formatting / tidying,  removed some supervised topics that were giving issues,

--- README.md
+++ README.md
@@ -1,19 +1,57 @@
 ## Tagger News
 
+Setup the environment
 ```
 git clone http://github.com/danrobinson/taggernews.git
 cd taggernews
 mkvirtualenv taggernews
+ln -s ~/.virtualenvs/taggernews/bin/activate ./activate
+echo export SECRET_KEY="PICK A SECRET KEY" >> /location/of/your/.virtualenvs/taggernews/bin/activate
+echo export DEBUG=True >> /location/of/your/.virtualenvs/taggernews/bin/activate
+```
+For some reason a 500 will be thrown on the admin page if you don't enable debug 
+
+Dependencies
+```
 pip install -r requirements.txt
-cd taggernews
 python manage.py migrate
 python manage.py createsuperuser
+```
+
+Start the server
+```
 python manage.py runserver
 open http://localhost:8000
 ```
+You should now see a tagger news clone start up with no data.
 
 For the admin interface:
-
 ```
 open http://localhost:8000/admin
+```
+**Data Gathering and Analysis**
+
+To import data run the commands:
+```
+python manage.py refresh_top_articles
+```
+This will start the parser which will continously run getting new articles and saving them to the db.
+
+Now to create an LDA model (Latent Dirichlet Allocation)
+In analyze_hn run:
+```
+python model_topics.py
+```
+
+This will provide you with a dictionary and gensim files.
+
+Plug these into the predictor
+```
+python predict_topics
+```
+
+
+Using this training set you can now put the machine to work
+```
+python manage.py tag_articles
 ```
--- articles/management/commands/refresh_top_articles.py
+++ articles/management/commands/refresh_top_articles.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from django.core.management.base import BaseCommand, CommandError
 import requests
 import sys
@@ -5,81 +6,98 @@ import sys
 from articles.models import Article
 
 from goose import Goose
+import json
 
-TOP_ARTICLES_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+TOP_ARTICLES_URL = 'https://hacker-news.firebaseio.com/v0/topstories/.json'
 ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/%s.json'
 
+
+class ArticleFetcher():
+
+    def fetch_single(self, fetch_id):
+        data = {fetch_id}
+        articles, _, _, = self.fetch(data)
+        if len(articles) is 1:
+            return articles[0]
+        else:
+            return None
+
+    def fetch(self, articles):
+        update_count = 0
+        create_count = 0
+        ret_list = []
+        for rank, article_id in enumerate(articles):
+            print(article_id)
+            article_info = requests.get(ITEM_URL % article_id).json()
+            try:
+                article = Article.objects.get(hn_id=article_id)
+                article.score = article_info.get('score')
+                article.number_of_comments = article_info.get('descendants')
+                article.rank = rank
+                if article.prediction_input is None:
+                    goose = Goose()
+                    url = article.article_url
+                    if not url:
+                        continue
+                    print("getting " + url)
+                    try:
+                        goosed_article = goose.extract(url=url)
+                        prediction_input = '%s|||\n\n%s' % (
+                            goosed_article.cleaned_text,
+                            goosed_article.meta_description,
+                        )
+                        article.prediction_input = prediction_input
+                    except:
+                        article.prediction_input = None
+                        print("Failed to load text for ", url)
+                article.save()
+                update_count += 1
+            except Article.DoesNotExist:
+                try:
+                    print("goosing")
+                    goose = Goose()
+                    url = article_info.get('url')
+                    if not url:
+                        continue
+                    print(url)
+                    goosed_article = goose.extract(url=url)
+                    prediction_input = '%s|||\n\n%s' % (
+                        goosed_article.cleaned_text,
+                        goosed_article.meta_description,
+                    )
+                    article = Article.objects.create(
+                        hn_id=article_id,
+                        title=article_info.get('title'),
+                        article_url=article_info.get('url'),
+                        score=article_info.get('score'),
+                        number_of_comments=article_info.get('descendants'),
+                        submitter=article_info.get('by'),
+                        timestamp=article_info.get('time'),
+                        rank=rank,
+                        prediction_input=prediction_input
+                    )
+                    create_count += 1
+                except Exception as e:
+                    sys.stderr.write(str(e))
+                    continue
+
+            ret_list.append(article)
+            if rank % 10 == 0:
+                message = "Added %s articles, updated %s articles..." % (
+                    create_count, update_count)
+                print(message)
+
+        Article.objects.exclude(hn_id__in=articles).update(rank=None)
+
+        return ret_list, create_count, update_count
+
+
 class Command(BaseCommand):
-  help = 'Closes the specified poll for voting'
+    help = 'Closes the specified poll for voting'
 
-  def handle(self, *args, **options):
-    top_article_ids = requests.get(TOP_ARTICLES_URL).json()
-    
-    update_count = 0 
-    create_count = 0
-    for rank, article_id in enumerate(top_article_ids):
-      print article_id
-      article_info = requests.get(ITEM_URL % article_id).json()
-      try:
-        article = Article.objects.get(hn_id=article_id)
-        article.score = article_info.get('score')
-        article.number_of_comments = article_info.get('descendants')
-        article.rank = rank
-        if article.prediction_input == None:
-          goose = Goose()
-          url = article.article_url
-          if (not url) or url[:13] == "https://arxiv":
-            continue
-          print "getting", url
-          try:
-            goosed_article = goose.extract(url=url)
-            prediction_input = '%s|||\n\n%s' % (
-              goosed_article.cleaned_text,
-              goosed_article.meta_description,
-            )
-            article.prediction_input = prediction_input
-          except:
-            article.prediction_input = None
-        article.save()
-        update_count += 1
-      except Article.DoesNotExist:
-        try:
-          print "goosing"
-          goose = Goose()
-          url = article_info.get('url')
-          if (not url) or url[:13] == "https://arxiv":
-            continue
-          print url
-          goosed_article = goose.extract(url=url)
-          prediction_input = '%s|||\n\n%s' % (
-            goosed_article.cleaned_text,
-            goosed_article.meta_description,
-          )
-          Article.objects.create(
-            hn_id=article_id,
-            title=article_info.get('title'),
-            article_url=article_info.get('url'),
-            score=article_info.get('score'),
-            number_of_comments=article_info.get('descendants'),
-            submitter=article_info.get('by'),
-            timestamp=article_info.get('time'),
-            rank=rank,
-            prediction_input=prediction_input
-          )
-          create_count += 1
-        except Exception as e:
-          sys.stderr.write(str(e))
+    def handle(self, *args, **options):
+        top_article_ids = requests.get(TOP_ARTICLES_URL).json()
 
-      
-      if rank % 10 == 0:
-        message = "Added %s articles, updated %s articles..." % (
-          create_count, update_count)
-        self.stdout.write(self.style.SUCCESS(message))
-    
-    Article.objects \
-      .exclude(hn_id__in=top_article_ids) \
-      .update(rank=None)
-
-    self.stdout.write(self.style.SUCCESS(
-      'Done. Added: %s, Updated: %s' % (create_count, update_count)))
-        
+        (articles, create_count, update_count) = ArticleFetcher.fetch(top_article_ids)
+        self.stdout.write(self.style.SUCCESS(
+            'Done. Added: %s, Updated: %s' % (create_count, update_count)))
