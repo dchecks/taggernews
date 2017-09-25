cde5582e356debe78b51cad45523c7d5dfddee25
Dan Checketts <dan.checketts@mahifx.com>
2017-09-26 00:15:39 +1300

    Made parsing a little bit nicer, added state and parsed datetime fields

--- analyze_hn/predict_topics.py
+++ analyze_hn/predict_topics.py
@@ -30,6 +30,8 @@ RANDOM_FOREST_NAME = "./models/predictions/randomforest_model"
 LOGISTIC_MODEL_NAME = "./models/predictions/logistic_model"
 
 C_VALUE = 1.0
+# If true will fetch articles that are not in the db, otherwise not include them in model
+FETCH_NOT_CACHED = True
 
 # Trained data
 label_df = DplyFrame(pd.read_csv("supervised_topics.csv"))
@@ -115,18 +117,7 @@ story_ids = []
 story_id_to_topics = {}
 
 
-def load_article(label, story_id):
-    article_id = story_id.item()
-    try:
-        article = Article.objects.get(hn_id=article_id)
-    except Article.DoesNotExist:
-        print("Article " + str(article_id) + " not found, fetching")
-        article = ArticleFetcher().fetch_single(article_id)
-
-    if article is None:
-        print("Skipping article: " + str(article_id))
-        return 0
-
+def add_article(label, story_id, article):
     labels.append(label)
     story_ids.append(story_id)
 
@@ -134,16 +125,46 @@ def load_article(label, story_id):
     topic_dicts.append(topic_dict)
     story_id_to_topics[story_id] = topic_dict
     print(label)
-    return 1
 
 
-for label, story_id in zip(label_df.topic, label_df.id):
-    load_article(label, story_id)
+def load_article(label_df):
+    skipped_due_state = 0
+    uncached_articles = []
+
+    for label, article_id in zip(label_df.topic, label_df.id):
+        article_id = article_id.item()
+        try:
+            article = Article.objects.get(hn_id=article_id)
+            if article.state is None:
+                # reparse it again just to get a state assigned
+                uncached_articles.append((label, article_id))
+            elif article.state is not 0:
+                skipped_due_state += 1
+            else:
+                add_article(label, article_id, article)
+        except Article.DoesNotExist:
+            uncached_articles.append((label, article_id))
+
+    print("Uncached articles: ", len(uncached_articles))
+
+    if FETCH_NOT_CACHED:
+        print("Fetching articles...")
+        for label, article_id in uncached_articles:
+            print("Fetching " + str(article_id))
+            article = ArticleFetcher().fetch_single(article_id)
+            if article.state is not 0:
+                print("Skipping article: " + str(article_id) + " due to state: " + article.state)
+            else:
+                add_article(label, article_id, article_id)
+    else:
+        print("Not attempting to fetch, set FETCH_NOT_CACHED if you want them, this can take a long time.")
+    print("Skipped due to invalid state: " + skipped_due_state)
+
 
 # Issue with requests in parallel mode
-# zipped_list = zip(label_df.topic, label_df.id)
+#zipped_list = zip(label_df.topic, label_df.id)
 # Load supervised data in parallel as parsing can be slow
-# Parallel(n_jobs=2)(delayed(load_article)(label, story_id) for label, story_id in zipped_list)
+#Parallel(n_jobs=2)(delayed(load_article)(label, story_id) for label, story_id in zipped_list)
 
 df = DplyFrame(pd.DataFrame({"labels": labels,
                              "story_id": story_ids,
--- articles/management/commands/refresh_top_articles.py
+++ articles/management/commands/refresh_top_articles.py
@@ -1,18 +1,21 @@
 from __future__ import print_function
-from django.core.management.base import BaseCommand, CommandError
 import requests
 import sys
 
-from articles.models import Article
-
+import time
+from django.core.management.base import BaseCommand
 from goose import Goose
-import json
+
+from articles.models import Article
 
 TOP_ARTICLES_URL = 'https://hacker-news.firebaseio.com/v0/topstories/.json'
 ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/%s.json'
 
 
-class ArticleFetcher():
+class ArticleFetcher:
+
+    def __init__(self):
+        pass
 
     def fetch_single(self, fetch_id):
         data = {fetch_id}
@@ -29,44 +32,70 @@ class ArticleFetcher():
         for rank, article_id in enumerate(articles):
             print(article_id)
             article_info = requests.get(ITEM_URL % article_id).json()
-            try:
-                article = Article.objects.get(hn_id=article_id)
-                article.score = article_info.get('score')
-                article.number_of_comments = article_info.get('descendants')
-                article.rank = rank
-                if article.prediction_input is None:
+
+            if article_info is None:
+                print("HN id unknown ", article_id)
+                article = Article.objects.create(
+                    hn_id=article_id,
+                    state=1,
+                    parsed=time.time()
+                )
+            elif (not url) or url[:13] == "https://arxiv":
+                print("No url for article ", article_id)
+                article = Article.objects.create(
+                    hn_id=article_id,
+                    state=2,
+                    parsed=time.time(),
+                    title=article_info.get('title'),
+                    article_url=article_info.get('url'), # still add it in case it's actually just bad
+                    score=article_info.get('score'),
+                    number_of_comments=article_info.get('descendants'),
+                    submitter=article_info.get('by'),
+                    timestamp=article_info.get('time'),
+                    rank=rank,
+                )
+            else:
+                url = article_info.get('url')
+                try:
+                    article = Article.objects.get(hn_id=article_id)
+                    article.score = article_info.get('score')
+                    article.number_of_comments = article_info.get('descendants')
+                    article.rank = rank
+                    if article.prediction_input is None or article.state is not 0:
+                        goose = Goose()
+                        print("getting " + url)
+                        try:
+                            goosed_article = goose.extract(url=url)
+                            prediction_input = '%s|||\n\n%s' % (
+                                goosed_article.cleaned_text,
+                                goosed_article.meta_description,
+                            )
+                            article.prediction_input = prediction_input
+                            article.state = 0
+                        except:
+                            article.prediction_input = None
+                            article.state = 3
+                            print("Failed to load text for ", url)
+                    update_count += 1
+                except Article.DoesNotExist:
+                    print("goosing")
                     goose = Goose()
-                    url = article.article_url
-                    if (not url) or url[:13] == "https://arxiv":
-                        continue
-                    print("getting " + url)
                     try:
+                        print(url)
                         goosed_article = goose.extract(url=url)
                         prediction_input = '%s|||\n\n%s' % (
                             goosed_article.cleaned_text,
                             goosed_article.meta_description,
                         )
-                        article.prediction_input = prediction_input
-                    except:
-                        article.prediction_input = None
-                        print("Failed to load text for ", url)
-                article.save()
-                update_count += 1
-            except Article.DoesNotExist:
-                try:
-                    print("goosing")
-                    goose = Goose()
-                    url = article_info.get('url')
-                    if (not url) or url[:13] == "https://arxiv":
-                        continue
-                    print(url)
-                    goosed_article = goose.extract(url=url)
-                    prediction_input = '%s|||\n\n%s' % (
-                        goosed_article.cleaned_text,
-                        goosed_article.meta_description,
-                    )
+                        state = 0
+                    except Exception as e:
+                        sys.stderr.write(str(e))
+                        state = 3
+
                     article = Article.objects.create(
                         hn_id=article_id,
+                        state=state,
+                        parsed=time.time(),
                         title=article_info.get('title'),
                         article_url=article_info.get('url'),
                         score=article_info.get('score'),
@@ -77,16 +106,13 @@ class ArticleFetcher():
                         prediction_input=prediction_input
                     )
                     create_count += 1
-                except Exception as e:
-                    sys.stderr.write(str(e))
-                    continue
-
+            article.save()
             ret_list.append(article)
             if rank % 10 == 0:
-                message = "Added %s articles, updated %s articles..." % (
-                    create_count, update_count)
+                message = "Added %s articles, updated %s articles..." % (create_count, update_count)
                 print(message)
 
+        # TODO eval need for this
         Article.objects.exclude(hn_id__in=articles).update(rank=None)
 
         return ret_list, create_count, update_count
